249f2af0b8a5fea732c7b06cc07ececb76f16294
Unknown <swilsondev@outlook.com>
2018-04-04 08:55:05 -0500

    Adding vuetify, and working on main template.

--- resources/views/layouts/dashboard/crud.blade.php
+++ resources/views/layouts/dashboard/crud.blade.php
@@ -1,6 +1,22 @@
 <html>
-    <head></head>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
     <body>
-        <div style="background-color:red"> @yield('content') </div>
+        <div id="main">
+            <v-app>
+                <v-toolbar color="indigo" dark fixed app>
+                    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                    <v-toolbar-title>Good Music 2.0</v-toolbar-title>
+                </v-toolbar>
+                <v-content>
+                    <div style="background-color:red"> @yield('content') </div>
+                    <v-btn color="success">Test</v-btn>
+                </v-content>
+            </v-app>
+        </div>
+
+        <script src="/js/app.js"></script>
     </body>
 </html>
