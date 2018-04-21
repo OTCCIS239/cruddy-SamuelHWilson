e23581cdfcd70b1cbd7f3695ecc52a84ef9ed3f8
Sam Wilson <swilsondev@outlook.com>
2018-04-21 13:35:00 -0500

    Adding CharacteristicsSummary [TL#3]

---
+++ resources/assets/js/components/characteristics/CharacteristicsSummary.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-chip v-for='char in chars' :key='char.id' class='primary white--text'>{{ char.name }}</v-chip>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            chars: { // { name: String, id: int }
+                type: Array,
+                default: []
+            }
+        }
+    }
+</script>
